ac3851b7a360dd66d472a9131966576ab15f7dc7
Thing Thinge <thing@something.com>
2022-02-07 16:11:21 +0100

    first full API iteration

---
+++ index.js
@@ -0,0 +1,176 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+const mysql = require('mysql2');
+const path = require('path');
+const cors = require("cors")
+var client;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors());
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname,'build', 'index.html'))
+})
+profiles = [{"name":"Warm","brightness":100,"temperature":1700},{"name":"Nap","brightness":20,"temperature":1700},{"name":"Harsh","brightness":100,"temperature":5000}]
+app.get('/profiles', cors(), function(req, res){
+  res.json(profiles)
+})
+
+app.listen(
+  PORT,
+  ()=> console.log(`it's alive on http://localhost:${PORT}`)
+
+)
+
+app.post('/createwhiteprofile/', (req,res) => {
+
+  console.log(req.body)
+  //const {id} = req.body;
+  const {brightness} = req.body;
+  const {temperature} = req.body;
+  //const {fluidity} = req.body;
+
+  console.log("Profile created with values brightness: "+ brightness +" temperature: "+ temperature );
+
+  client.write('WHITEPARAMCOMMAND:'+brightness+":"+temperature+'\r\n');
+  client.pipe(client);
+
+  res.status(200).send({
+      lul: 'lullers dude',
+      size:'large'
+  })
+})
+
+app.post('/createwhiteprofile', (req,res) => {
+
+  console.log(req.body)
+  const {id} = req.body;
+  const {brightness} = req.body;
+  const {temperature} = req.body;
+  const {fluidity} = req.body;
+
+  console.log("Profile created with values ID:"+id+" Brightness:"+brightness+" temp: "+temperature+" Fluidity:"+fluidity);
+
+  client.write('WHITEPARAMCOMMAND:'+brightness+":"+temperature+":"+fluidity+'\r\n');
+  client.pipe(client);
+
+  res.status(201).send({
+      confirm: 'White light parameters set'
+
+  })
+})
+
+app.post('/shutdown', (req,res) => {
+
+  console.log(req.body)
+
+  console.log("Shutdown command received");
+
+  client.write('SHUTDOWNCOMMAND\r\n');
+  client.pipe(client);
+
+  res.status(201).send({
+      confirm: 'shutdown order received'
+  })
+})
+
+app.post('/turnon', (req,res) => {
+
+  console.log(req.body)
+
+  console.log("Turn on command received");
+
+  client.write('TURNONCOMMAND\r\n');
+  client.pipe(client);
+
+  res.status(201).send({
+      confirm: 'turn on order received'
+  })
+})
+
+app.get('/profiles', (req,res) => {
+
+  console.log(req.body)
+
+  console.log("Frontend requested profiles");
+
+  res.status(200).send({
+      confirm: 'turn on order received'
+  })
+})
+
+
+const net = require('net');
+const server = net.createServer((c) => {
+  client = c;
+  c.on('error', function (exc) {
+    console.log("Client disconnected through error: " + exc);
+});
+  console.log('client connected');
+  c.on('end', () => {
+    console.log('client disconnected');
+  });
+
+});
+server.on('error', (err) => {
+  throw err;
+});
+
+server.listen(8081, () => {
+  console.log('server bound');
+});
+/*
+const db = mysql.createConnection({
+  host : 'localhost',
+  user : 'root',
+  password : 'password',
+  insecureAuth: true
+})
+
+db.connect((err) => {
+  if(err){
+    throw err;
+  }
+  console.log("in like flynn");
+})
+*/
+
+
+
+
+
+
+/*
+
+
+
+
+app.post('/creatergbprofile', (req,res) => {
+
+  res.status(200).send({
+      lul: 'lullers dude',
+      size:'large'
+  })
+})
+
+app.post('/setlightparams/:id', (req,res) => {
+  res.status(200).send({
+      lul: 'lullers dude',
+      size:'large'
+  })
+})
+
+app.get('/status/:id', (req,res) => {
+  res.status(200).send({
+      lul: 'lullers dude',
+      size:'large'
+  })
+})
+
+/*
+
+
+*/
